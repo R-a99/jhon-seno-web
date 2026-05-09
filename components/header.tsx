4eaad3fc37c1730e67782bf4ec000f2daf9e7dfb
fatkhul <dhany.el.99@gmail.com>
2026-05-09 19:38:51 +0700

    logo

--- components/header.tsx
+++ components/header.tsx
@@ -25,7 +25,7 @@ export function Header() {
             {/* Ukuran logo diperkecil agar proporsional dengan tinggi header */}
             <div className="relative h-9 w-20 md:h-10 md:w-24">
               <Image 
-                src="/images/logo.png" 
+                src="/images/logo.jpeg" 
                 alt="Jhon Seno Company Logo" 
                 fill
                 className="object-contain hover:opacity-90 transition-opacity"
